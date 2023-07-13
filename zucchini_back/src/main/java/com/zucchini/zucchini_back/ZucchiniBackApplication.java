1f4761658452506822c95b5d7ea0f81a3b936cd6
dkgusdkfk <dkgusdkfk@gmail.com>
2023-07-13 13:25:18 +0900

    [zucchini]init: backend 시작

---
+++ zucchini_back/src/main/java/com/zucchini/zucchini_back/ZucchiniBackApplication.java
@@ -0,0 +1,13 @@
+package com.zucchini.zucchini_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZucchiniBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZucchiniBackApplication.class, args);
+	}
+
+}
---
+++ zucchini_back/src/test/java/com/zucchini/zucchini_back/ZucchiniBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zucchini.zucchini_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZucchiniBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
