c5a127ee2e70af479efb009fedd3d08ac6d95963
heeya1morestep <heeya1morestep@gmail.com>
2023-07-18 17:46:10 +0900

    [Zucchini-31] Feat : 로그인 페이지 제작 draft

--- zucchini_frontend/src/pages/LogIn.tsx
+++ zucchini_frontend/src/pages/LogIn.tsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export default function LogIn() {
-  return <div>LogIn</div>;
+  const [values, setValues] = useState({ id: "", pw: "" });
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    setValues(data);
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>로그인</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <p>
+          <input placeholder="아이디" {...register("id")}></input>
+        </p>
+        <p>
+          <input placeholder="비밀번호" {...register("pw")}></input>
+        </p>
+        <button>로그인</button>
+      </form>
+    </div>
+  );
 }
