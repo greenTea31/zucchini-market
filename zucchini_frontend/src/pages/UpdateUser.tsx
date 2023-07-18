5932a5d586e2947ff5364ca5dd4717fb107f7ebb
이상호 <istrangeho@gmail.com>
2023-07-18 14:32:07 +0900

    Revert "Merge branch 'Zucchini-34' into 'develop'"
    This reverts merge request !2

--- zucchini_frontend/src/pages/UpdateUser.tsx
+++
@@ -1,7 +0,0 @@
-export default function UpdateUser() {
-  return (
-    <div>
-      Update User
-    </div>
-  )
-}
--- zucchini_frontend/src/router.tsx
+++ zucchini_frontend/src/router.tsx
@@ -1,17 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
-import UpdateUser from "./pages/UpdateUser";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [
-      {
-        path: "/updateuser",
-        element: <UpdateUser />,
-      },
-    ],
   },
 ]);
 
