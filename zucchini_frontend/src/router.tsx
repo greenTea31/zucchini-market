7d360abc126549a3a03f77b8a3ca4321c32a5533
이상호 <istrangeho@gmail.com>
2023-07-18 14:27:59 +0900

    [Zucchini-35] Feat: router.tsx 파일에 signup 경로 추가 pages 폴더에 signup.tsx 파일 추가

--- zucchini_frontend/src/router.tsx
+++ zucchini_frontend/src/router.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
+import SignUp from "./pages/SignUp";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
   },
 ]);
 
