2bf2d011649022fb8d86796868a1febaed633567
heeya1morestep <heeya1morestep@gmail.com>
2023-07-18 14:27:59 +0900

    [Zucchini-31]Feat: router.tsx 파일에 login 경로 추가 pages 폴더에 LogIn파일 추가

--- zucchini_frontend/src/router.tsx
+++ zucchini_frontend/src/router.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
+import LogIn from "./pages/LogIn";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "login",
+        element: <LogIn />,
+      },
+    ],
   },
 ]);
 
