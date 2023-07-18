a9cd9a661f67e78420489ff4e41ff9d4bfeed148
dkgusdkfk <dkgusdkfk@gmail.com>
2023-07-18 16:59:25 +0900

    [Zucchini-56] Feat : 로그인 시 Token 발급

--- zucchini_backend/src/main/java/com/zucchini/domain/user/api/UserController.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/user/api/UserController.java
@@ -1,9 +1,12 @@
 package com.zucchini.domain.user.api;
 
+import com.zucchini.domain.user.dto.request.LoginRequest;
 import com.zucchini.domain.user.service.UserService;
 import com.zucchini.domain.user.dto.request.AddUserRequest;
+import com.zucchini.global.domain.TokenDto;
 import com.zucchini.global.util.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,11 @@ public class UserController {
     public String signUp(@RequestBody AddUserRequest user) {
         userService.addUser(user);
         return "회원가입 완료";
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(userService.login(loginRequest));
     }
 
 }
--- zucchini_backend/src/main/java/com/zucchini/domain/user/service/UserService.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.zucchini.domain.user.service;
 import com.zucchini.domain.user.dto.request.AddUserRequest;
 import com.zucchini.domain.user.dto.request.LoginRequest;
 import com.zucchini.domain.user.dto.response.FindUserResponse;
+import com.zucchini.global.domain.TokenDto;
 
 public interface UserService {
 
@@ -10,7 +11,7 @@ public interface UserService {
     void addUser(AddUserRequest user);
     void modifyUser(int userNo);
     void removeUser(int userNo);
-    void login(LoginRequest loginRequest);
+    TokenDto login(LoginRequest loginRequest);
     void logout();
 
 }
