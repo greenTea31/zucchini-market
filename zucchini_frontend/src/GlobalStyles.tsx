86e38237704f98b92499ce719a259bef21e4a980
boyoun1113 <boyoun1113@naver.com>
2023-07-18 17:42:06 +0900

    [Zucchini-Front] Feat: 회원정보수정 페이지 폰트 적용 글로벌 css 적용 회원정보수정 페이지 제작

---
+++ zucchini_frontend/src/GlobalStyles.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=swap');
+
+  @font-face{
+    font-family: 'IBM Plex Sans KR', sans-serif;
+    src: url("./assets/fonts/IBMPlexSansKR-Regular.ttf") format('ttf');
+  }
+
+  * {
+    font-family: 'IBM Plex Sans KR', sans-serif;
+  }
+
+`;
+
+export default GlobalStyles;
