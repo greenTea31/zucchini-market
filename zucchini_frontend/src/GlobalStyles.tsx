580e63f4fe8c98c281e08737e9681cf7f748f00a
이상호 <istrangeho@gmail.com>
2023-07-19 12:27:45 +0900

    [Zucchini-35] Feat: 헤더 추가 헤더 메뉴기능을 제외하곤 구현 완료

--- zucchini_frontend/src/GlobalStyles.tsx
+++ zucchini_frontend/src/GlobalStyles.tsx
@@ -3,6 +3,13 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
+
+  a {
+    text-decoration: none;
+    color: black;
+    outline: none;
+  }
+
   @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=swap');
 
   @font-face{
---
+++ zucchini_frontend/src/components/Header.tsx
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import Navigation from "./Navigation";
+import Menu from "./Menu";
+import { useState, useEffect } from "react";
+import MenuWindow from "./MenuWindow";
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 64px;
+  display: flex;
+  justify-content: space-between;
+  background-color: #aacb73;
+  align-items: center;
+  padding: 0 64px;
+  box-sizing: border-box;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+`;
+
+const Logo = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+  margin-right: 80px;
+  user-select: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    console.log(isMenuOpen);
+  }, [isMenuOpen]);
+
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <Logo>애호박마켓</Logo>
+        <Navigation
+          list={[
+            {
+              navLink: "/items",
+              navName: "상품보기",
+            },
+            {
+              navLink: "/conference",
+              navName: "화상통화",
+            },
+            {
+              navLink: `/myPage`,
+              navName: "마이페이지",
+            },
+          ]}
+        />
+      </HeaderLeft>
+
+      <HeaderRight>
+        <Menu toggle={setIsMenuOpen} />
+        {isMenuOpen && <MenuWindow />}
+      </HeaderRight>
+    </HeaderContainer>
+  );
+}
