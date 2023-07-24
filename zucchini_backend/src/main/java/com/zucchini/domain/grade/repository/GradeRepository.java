51f26a2dcd309978498bf262ed7b4dced3ace8db
조은비 <harunz@naver.com>
2023-07-24 13:09:56 +0900

    [Zucchini-165] Feat : Grade Repository 생성 Grade Repository 생성

--- zucchini_backend/src/main/java/com/zucchini/domain/grade/repository/GradeRepository.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/grade/repository/GradeRepository.java
@@ -4,4 +4,6 @@ import com.zucchini.domain.grade.domain.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GradeRepository extends JpaRepository<Grade, Integer> {
+
+
 }
