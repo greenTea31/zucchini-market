14ab7736201bf84a78723c81b86f27e259fa9a1d
dkgusdkfk <dkgusdkfk@gmail.com>
2023-07-25 12:46:14 +0900

    [Zucchini-149] Feat : 아이템 생성 아이템 생성(이미지 제외) 구현

--- zucchini_backend/src/main/java/com/zucchini/domain/item/repository/DateRepository.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/item/repository/DateRepository.java
@@ -4,4 +4,5 @@ import com.zucchini.domain.item.domain.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DateRepository extends JpaRepository<Date, Integer> {
+
 }
