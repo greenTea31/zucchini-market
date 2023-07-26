b8be24f6cf82c196390ab4978e3980bed7738bd5
조은비 <harunz@naver.com>
2023-07-26 12:32:57 +0900

    [Zucchini-170] Feat : Category Service 구현 Category 전체 조회, Category에 해당하는 아이템 목록 조회 구현

--- zucchini_backend/src/main/java/com/zucchini/domain/category/repository/ItemCategoryRepository.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/category/repository/ItemCategoryRepository.java
@@ -2,6 +2,7 @@ package com.zucchini.domain.category.repository;
 
 import com.zucchini.domain.category.domain.ItemCategory;
 import com.zucchini.domain.category.domain.ItemCategoryId;
+import com.zucchini.domain.item.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,8 +14,15 @@ public interface ItemCategoryRepository extends JpaRepository<ItemCategory, Item
     /**
      * 카테고리에 해당하는 아이템 목록 조회
      */
-    List<ItemCategory> findAllByCategory(String category);
+    @Query(value = "SELECT i FROM ItemCategory ic " +
+            "JOIN ic.category c " +
+            "JOIN ic.item i " +
+            "WHERE c.category = :category")
+    List<Item> findAllByCategory(String category);
 
+    /**
+     * 해당하는 아이템이 속한 카테고리 전부 삭제
+     */
     @Modifying
     @Query(value = "delete from Item_category where Item_category.item_no = :itemNo", nativeQuery = true)
     void deleteByItemNo(int itemNo);
