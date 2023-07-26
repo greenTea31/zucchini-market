32d2bf85c87f33b6385bd8e941b8be6a8b8f4e94
조은비 <harunz@naver.com>
2023-07-26 17:25:21 +0900

    [Zucchini-172] Feat : Video Entity 생성 Video Entity 생성

--- zucchini_backend/src/main/java/com/zucchini/domain/video/domain/Video.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/video/domain/Video.java
@@ -5,7 +5,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
