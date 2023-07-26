47029f0a75030b48bb91a58bae7c24948763d164
이상호 <istrangeho@gmail.com>
2023-07-26 16:41:54 +0900

    [Zucchihi-123] Feat: React Query를 사용한 로그인 구현 - 로그인 기능 구현 완료 - 토큰을 사용해 정보 가져오기 구현중

--- zucchini_frontend/src/index.tsx
+++ zucchini_frontend/src/index.tsx
@@ -3,15 +3,21 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import GlobalStyles from "./GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import GlobalFonts from "./assets/styles/fonts";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     {/* <GlobalFonts /> */}
     <GlobalStyles />
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- zucchini_frontend/src/utils/axios.ts
+++ zucchini_frontend/src/utils/axios.ts
@@ -1,1 +1,25 @@
 import Axios from "axios";
+
+const BASE_URL = "http://localhost:8080/";
+
+const axios = Axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const http = {
+  get: function get(url: string) {
+    return axios.get(url).then((res) => res.data);
+  },
+  post: async function post(url: string, body: any) {
+    return await axios.post(url, body).then((res) => res.data);
+  },
+  put: function put(url: string, body: any) {
+    return axios.put(url, body).then((res) => res.data);
+  },
+  delete: function remove(url: string) {
+    return axios.delete(url).then((res) => res.data);
+  },
+};
