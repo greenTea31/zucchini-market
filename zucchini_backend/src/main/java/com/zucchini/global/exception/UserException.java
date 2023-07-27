78bccb31480c802e74dd96432847d42f3166d016
dkgusdkfk <dkgusdkfk@gmail.com>
2023-07-27 17:25:05 +0900

    [Zucchini-148] Feat : GlobalExceptionHandler GlobalExceptionHandler 구현

--- zucchini_backend/src/main/java/com/zucchini/global/exception/UserException.java
+++ zucchini_backend/src/main/java/com/zucchini/global/exception/UserException.java
@@ -1,6 +1,7 @@
 package com.zucchini.global.exception;
 
 public class UserException extends RuntimeException {
+
     public UserException() {}
     public UserException(String msg) {
         super(msg);
