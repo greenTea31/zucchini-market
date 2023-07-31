e01cbfdc353b8b1d02490deaaf843e74075206cd
eunbypa <harunz@naver.com>
2023-08-01 00:51:44 +0900

    [Zucchini-261] Feat : ItemService Date status 변경 메서드 추가 예약 성공 시 해당 날짜 status 1로 변경 메서드, 예약된 날짜와 일치하는 판매 날짜 모두 status 2로 변경 메서드 추가

--- zucchini_backend/src/main/java/com/zucchini/domain/item/service/ItemService.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/item/service/ItemService.java
@@ -4,6 +4,7 @@ import com.zucchini.domain.item.dto.request.ItemRequest;
 import com.zucchini.domain.item.dto.response.FindItemListResponse;
 import com.zucchini.domain.item.dto.response.FindItemResponse;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ItemService {
@@ -16,5 +17,7 @@ public interface ItemService {
     void modifyStatusDeal(int itemNo, String buyer);
     void modifyStatusConfirmation(int itemNo);
     void modifyStatusCancel(int itemNo);
+    void modifyDateReservation(int itemNo, Date selectDate);
+    void modifyDateStatus(Date selectDate);
 
 }
