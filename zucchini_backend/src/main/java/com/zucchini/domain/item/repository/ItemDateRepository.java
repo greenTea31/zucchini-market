29dcda35778d336ecd42b54f1996c362131fe7bc
eunbypa <harunz@naver.com>
2023-08-01 00:48:09 +0900

    [Zucchini-261] Feat : ItemDateRepository 메서드 추가 아이템 번호와 날짜로 ItemDate 조회 메서드, 회원 번호와 날짜에 해당하는 ItemDate 리스트 조회 메서드 추가

--- zucchini_backend/src/main/java/com/zucchini/domain/item/repository/ItemDateRepository.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/item/repository/ItemDateRepository.java
@@ -2,11 +2,20 @@ package com.zucchini.domain.item.repository;
 
 import com.zucchini.domain.item.domain.ItemDate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface ItemDateRepository extends JpaRepository<ItemDate, Integer> {
 
     void deleteByItemNo(int itemNo);
+
+    ItemDate searchItemDateByItemNoAndDate(int itemNo, Date selectDate);
+
+    @Query("SELECT d FROM ItemDate d " +
+            "JOIN Item i " +
+            "WHERE i.seller.no = :userNo and d.date = :selectDate")
+    List<ItemDate> searchItemDatesByUser(int userNo, Date selectDate);
 
 }
