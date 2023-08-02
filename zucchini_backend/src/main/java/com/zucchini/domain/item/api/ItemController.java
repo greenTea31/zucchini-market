64acae902040f9384a0be2c51bdb2d1706898398
dkgusdkfk <dkgusdkfk@gmail.com>
2023-08-02 10:29:48 +0900

    [Zucchini-265] Comment : 상품 주석 상품관련 주석 작성

--- zucchini_backend/src/main/java/com/zucchini/domain/item/api/ItemController.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/item/api/ItemController.java
@@ -23,6 +23,8 @@ public class ItemController {
      * 상품 전체 조회
      * @param keyword : 검색어
      * @return List<FindItemListResponse> : 상품 전체 리스트
+     * 200 : 조회 성공
+     * 500 : 서버 에러
      */
     @GetMapping
     public ResponseEntity<List<FindItemListResponse>> findItem(@RequestParam String keyword) {
@@ -33,6 +35,9 @@ public class ItemController {
      * 상품 상세 조회
      * @param itemNo : 아이템 번호 (PK)
      * @return FindItemResponse : 상품 정보
+     * 200 :조회 성공
+     * 404 : 대상 없음
+     * 500 : 서버 에러
      */
     @GetMapping("/{itemNo}")
     public ResponseEntity<FindItemResponse> findItem(@PathVariable int itemNo) {
@@ -43,6 +48,10 @@ public class ItemController {
      * 상품 등록
      * @param item : 입력된 상품 정보
      * @return itemNo : 생성된 상품 번호(PK)
+     * 201 : 등록 성공
+     * 400 : 잘못된 요청 (필수 값이 채워지지 않거나 이상한 값 입력, 날짜 중복)
+     * 403 : 권한 없음 (로그인 하지 않음)
+     * 500 : 서버 에러
      */
     @PostMapping("/add")
     public ResponseEntity<Integer> addItem(@Valid @RequestBody ItemRequest item) {
@@ -55,6 +64,11 @@ public class ItemController {
      * @param itemNo : 상품 번호 (PK)
      * @param item : 수정된 상품 정보
      * @return
+     * 200 : 수정 성공
+     * 400 : 잘못된 요청
+     * 403 : 권한 없음 (로그인 하지 않음, 본인의 상품이 아님)
+     * 404 : 대상 없음
+     * 500 : 서버 에러
      */
     @PutMapping("/{itemNo}")
     public ResponseEntity<Void> modifyItem(@PathVariable int itemNo, @Valid @RequestBody ItemRequest item) {
@@ -66,6 +80,11 @@ public class ItemController {
      * 상품 삭제
      * @param itemNo : 상품 번호 (PK)
      * @return
+     * 200 : 삭제 성공
+     * 400 : 거래중인 상품 삭제 불가능
+     * 403 : 권한 없음 (로그인하지 않음, 본인의 상품이 아님)
+     * 404 : 대상 없음
+     * 500 : 서버 에러
      */
     @DeleteMapping("/{itemNo}")
     public ResponseEntity<Void> removeItem(@PathVariable int itemNo) {
@@ -78,6 +97,11 @@ public class ItemController {
      * @param itemNo : 상품 번호 (PK)
      * @param buyer : 구매자 아이디
      * @return
+     * 200 : 상태 변경 성공
+     * 400 : 이미 거래중이거나 완료된 상품
+     * 403 : 권한 없음 (로그인하지 않음, 본인의 상품이 아님)
+     * 404 : 대상 없음
+     * 500 : 서버 에러
      */
     @PutMapping("/{itemNo}/deal")
     public ResponseEntity<Void> dealItem(@PathVariable int itemNo, @RequestParam String buyer) {
@@ -89,6 +113,11 @@ public class ItemController {
      * 구매 확정, 상품 상태 (예약중 -> 판매완료) 변경 (1 -> 2)
      * @param itemNo : 상품 번호 (PK)
      * @return
+     * 200 : 상태 변경 성공
+     * 400 : 거래중인 상품 아님
+     * 403 : 권한 없음 (로그인하지 않음, 본인의 상품이 아님)
+     * 404 : 대상 없음
+     * 500 : 서버 에러
      */
     @PutMapping("/{itemNo}/confirmation")
     public ResponseEntity<Void> confirmationItem(@PathVariable int itemNo) {
@@ -100,6 +129,11 @@ public class ItemController {
      * 예약 취소, 상품 상태 (예약중 -> 판매중) 변경 (1 -> 0)
      * @param itemNo : 상품 번호 (PK)
      * @return
+     * 200 : 상태 변경 성공
+     * 400 : 거래중인 상품 아님
+     * 403 : 권한 없음 (로그인하지 않음, 본인의 상품이 아님)
+     * 404 : 대상 없음
+     * 500 : 서버 에러
      */
     @PutMapping("/{itemNo}/cancel")
     public ResponseEntity<Void> cancelItem(@PathVariable int itemNo) {
