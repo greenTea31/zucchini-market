bced8cf18e5c60c83aa3277e569d88b20a4e6ef7
baiksungil <sibaek31@gmail.com>
2023-08-04 10:36:51 +0900

    [Zucchini-248] Feat : 실시간 채팅 기능 구현

--- zucchini_frontend/src/components/Chat/Chatting.tsx
+++ zucchini_frontend/src/components/Chat/Chatting.tsx
@@ -1,13 +1,23 @@
 import styled from "styled-components";
 import MesssageEach from "../List/MessageEach";
+import Imessage from "../../types/Imessage";
 
-export default function Chatting() {
+interface Iprops {
+  message: Imessage;
+}
+
+export default function Chatting(props: Iprops) {
   return (
     <ChatBubble>
-      {[1, 2, 3, 4, 5].map((e, i) => (
+      <MesssageEach message={props.message} />
+      {/* {[1, 2, 3, 4, 5].map((e, i) => (
         // props로 작성자 정보 보내주기
         <MesssageEach send={e} />
-      ))}
+      ))} */}
+
+      {/* {messages.map((message, index) => (
+        <MesssageEach key={index} message={message} />
+      ))} */}
     </ChatBubble>
   );
 }
--- zucchini_frontend/src/components/List/MessageEach.tsx
+++ zucchini_frontend/src/components/List/MessageEach.tsx
@@ -1,14 +1,19 @@
 import RightChat from "../Chat/RightChat";
 import LeftChat from "../Chat/LeftChat";
+import Imessage from "../../types/Imessage";
 
-export default function MesssageEach({ send }: any) {
+interface Iprops {
+  message: Imessage;
+}
+
+export default function MesssageEach(props: Iprops) {
   return (
     <div>
       {/* 조건부에 본인인지 상대방인지 알아보는 로직 */}
-      {send % 2 === 0 ? (
-        <RightChat>되나........된다!</RightChat>
+      {props.message.sender === "SSAFY" ? (
+        <RightChat>{props.message.content}</RightChat>
       ) : (
-        <LeftChat>어쩌고 저쩌꼬</LeftChat>
+        <LeftChat>{props.message.content}</LeftChat>
       )}
     </div>
   );
