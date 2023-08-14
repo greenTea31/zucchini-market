f710d9a2ea38a45dbecd2e27c844acfd7ede4ef1
eunbypa <harunz@naver.com>
2023-08-15 00:02:42 +0900

    [AT] Fix : backend 빌드 에러 수정중

--- zucchini_backend/src/test/java/com/zucchini/zucchini_back/ZucchiniBackApplicationTests.java
+++ zucchini_backend/src/test/java/com/zucchini/zucchini_back/ZucchiniBackApplicationTests.java
@@ -3,7 +3,7 @@ package com.zucchini.zucchini_back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ZucchiniBackApplicationTests.class)
 class ZucchiniBackApplicationTests {
 
 	@Test
