cfc8adf26229f66e1b9cff84a5d7faa976bf3c8e
boyoun1113 <boyoun1113@naver.com>
2023-08-16 15:20:17 +0900

    [Zucchini-372] Feat: Modal 2개 추가 생성 ConferenceRoom에 들어가는 구매확정 관련 Modal 2개 생성

--- zucchini_frontend/src/components/Schedule/ScheduleEach.tsx
+++ zucchini_frontend/src/components/Schedule/ScheduleEach.tsx
@@ -26,7 +26,7 @@ export default function ScheduleEach(props: IProps) {
         {moment(props?.item?.confirmedDate).format("hh시 mm분")}
       </TimeP>
       <TitleP>{props?.item?.title}</TitleP>
-      {date1.diff(now, "minute") >= -10 && date1.diff(now, "minute") <= 30 ? (
+      {/* {date1.diff(now, "minute") >= -10 && date1.diff(now, "minute") <= 30 ? (
         <Link
           to={`/conference/${props?.item?.conferenceNo}`}
           state={{ title: `${props?.item?.title}` }}
@@ -37,7 +37,15 @@ export default function ScheduleEach(props: IProps) {
         </Link>
       ) : (
         <NoneDiv></NoneDiv>
-      )}
+      )} */}
+      <Link
+        to={`/conference/${props?.item?.conferenceNo}`}
+        state={{ title: `${props?.item?.title}` }}
+      >
+        <Button kind={"small"} Variant={"pinkTonal"}>
+          참여
+        </Button>
+      </Link>
     </VideoDiv>
   );
 }
