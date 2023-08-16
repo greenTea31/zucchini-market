bf11df34f21ac8e3c847390d95ecc7b66309013d
eunbypa <harunz@naver.com>
2023-08-17 03:32:42 +0900

    [Zucchini-417] Fix : 회원 탈퇴 시 에러 수정 회원 탈퇴 시 해당 회원과 연관된 report 전부 삭제하고 탈퇴하도록 수정

--- zucchini_backend/src/main/java/com/zucchini/domain/report/repository/ReportRepository.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/report/repository/ReportRepository.java
@@ -2,10 +2,9 @@ package com.zucchini.domain.report.repository;
 
 import com.zucchini.domain.report.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 public interface ReportRepository extends JpaRepository<Report, Integer> {
@@ -16,5 +15,9 @@ public interface ReportRepository extends JpaRepository<Report, Integer> {
 
     // room_no 입력받아서 해당 방을 신고하는 report가 존재 여부 확인함
     boolean existsByRoomNo(int roomNo);
+
+    @Modifying
+    @Query(value = "DELETE FROM Report r WHERE r.reported = :userId OR r.reporter = :userId")
+    void deleteAllByUser(String userId);
 
 }
