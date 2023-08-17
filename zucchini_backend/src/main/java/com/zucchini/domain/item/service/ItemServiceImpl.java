5ddfa5de87f930c038e6c39481565776eee07771
dkgusdkfk <dkgusdkfk@gmail.com>
2023-08-17 17:08:39 +0900

    [Zucchini-382] Fix: addItem date sort addItem date sort

--- zucchini_backend/src/main/java/com/zucchini/domain/item/service/ItemServiceImpl.java
+++ zucchini_backend/src/main/java/com/zucchini/domain/item/service/ItemServiceImpl.java
@@ -208,7 +208,9 @@ public class ItemServiceImpl implements ItemService {
 
         // 화상 가능한 날짜 리스트
         List<Date> inputDateList = item.getDateList();
-        Collections.sort(inputDateList);
+        if (inputDateList != null) {
+            Collections.sort(inputDateList);
+        }
 
         // inputDateList와 reservatedList의 요소중 하나라도 겹치는게 있으면 예외를 발생시킴
         List<Date> overlapDateList = reservationRepository.findOverlapDatesByUserAndDates(user, inputDateList);
